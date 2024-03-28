8c0ea58df018162d815cd7c3ec73a83e2c91e418
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-03-28 10:06:05 +0530

    resolve restaurent menu

--- src/components/RestaurentMenu.js
+++ src/components/RestaurentMenu.js
@@ -10,19 +10,19 @@ const RestaurentMenu = () => {
   const { resId } = useParams();
 
   const resInfo = useRestaurentMenu(resId);
+  console.log(resInfo);
+
 
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines, costForTwoMessage, avgRating } =
-    resInfo?.cards[0]?.card?.card?.info;
+    resInfo?.cards[2]?.card?.card?.info;
 
   const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
-
-  console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
   const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
